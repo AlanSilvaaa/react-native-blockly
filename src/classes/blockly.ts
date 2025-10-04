3a92299871f545d920d55aa2f516241e216232eb
alan <asilvar4@correo.uss.cl>
2025-10-03 22:26:15 -0300

    separate blockly class into a file

---
+++ src/classes/blockly.ts
@@ -0,0 +1,38 @@
+import type { Block, Toolbox } from "../types";
+
+export class Blockly {
+  blocks: Block[] = [];
+  toolbox: Toolbox = { kind: '', contents: [] };
+  constructor(public name: string) { }
+
+  greet(): string {
+    return `Hello, ${this.name}!`;
+  }
+
+  // TODO: create a new method called createBlocks that takes an array of Block objects
+  // and adds them to the blocks array
+  createBlocks(blocks: Block[]): void {
+    this.blocks.push(...blocks);
+  }
+
+  /**
+   * Creates a blockly block and add it to the blocks array.
+   * 
+   * @param block - Block object to be added to the blocks array
+   */
+  createBlock(block: Block): void {
+    this.blocks.push(block);
+  }
+
+  getBlocks(): Block[] {
+    return this.blocks;
+  }
+
+  setToolbox(toolbox: Toolbox): void {
+    this.toolbox = toolbox;
+  }
+
+  getToolbox(): Toolbox {
+    return this.toolbox;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,43 +2,10 @@ import WebView from "react-native-webview";
 import type { Block, Toolbox } from "./types";
 import { parseBlocksToString, parseToolboxToString } from "./parseToString";
 import { stringToBlockly } from "./stringToBlockly";
+import { Blockly } from "./classes/blockly";
 
-export class Blockly {
-  blocks: Block[] = [];
-  toolbox: Toolbox = { kind: '', contents: [] };
-  constructor(public name: string) { }
-
-  greet(): string {
-    return `Hello, ${this.name}!`;
-  }
-
-  // TODO: create a new method called createBlocks that takes an array of Block objects
-  // and adds them to the blocks array
-  createBlocks(blocks: Block[]): void {
-    this.blocks.push(...blocks);
-  }
-
-  /**
-   * Creates a blockly block and add it to the blocks array.
-   * 
-   * @param block - Block object to be added to the blocks array
-   */
-  createBlock(block: Block): void {
-    this.blocks.push(block);
-  }
-
-  getBlocks(): Block[] {
-    return this.blocks;
-  }
-
-  setToolbox(toolbox: Toolbox): void {
-    this.toolbox = toolbox;
-  }
-
-  getToolbox(): Toolbox {
-    return this.toolbox;
-  }
-}
+// Export the Blockly class so it can be imported by users
+export { Blockly };
 
 // Define props type for the component
 type BlocklyViewProps = {
